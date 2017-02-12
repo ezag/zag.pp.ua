8a36e70068c3ccb5dc7c9c8664706185dc6260c2
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2017-02-12 19:36:35 +0200

    Support IndieAuth

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,10 +26,60 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = ((
+        'BitBucket',
+        'https://bitbucket.org/ezag/',
+    ), (
+        'CodinGame',
+        'https://www.codingame.com'
+        '/profile/35bede9fa9bcc51d0a3449206cfbe675779671',
+    ), (
+        'Coursera',
+        'https://www.coursera.org/user/i/61c141a4b3a5d44868479a8073424dd0',
+    ), (
+        'GitHub',
+        'https://github.com/ezag',
+    ), (
+        'GitLab',
+        'https://gitlab.com/e.zag',
+    ), (
+        'Google+',
+        'https://plus.google.com/u/0/102533519110366328367',
+    ), (
+        'HackerRank',
+        'https://www.hackerrank.com/e_zag',
+    ), (
+        'Instagram',
+        'https://www.instagram.com/e.zag/',
+    ), (
+        'Kaggle',
+        'https://www.kaggle.com/ezagorodniy',
+    ), (
+        'LinkedIn',
+        'https://www.linkedin.com/in/e-zag/',
+    ), (
+        'LiveJournal',
+        'http://zag87.livejournal.com/',
+    ), (
+        'Quora',
+        'https://www.quora.com/profile/Eugen-Zagorodniy',
+    ), (
+        'StackExchange',
+        'http://stackexchange.com/users/198746/zag',
+    ), (
+        'Twitter',
+        'https://twitter.com/e4zag',
+    ), (
+        'YouTube',
+        'https://www.youtube.com/channel/UCiqZm9vM8SjcOt80mdttgxg',
+    ), (
+        'ВКонтакте',
+        'https://vk.com/e.zagorodniy',
+))
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+THEME = 'zag'
